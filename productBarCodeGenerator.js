57685155dbd32135457ea79112aca175602ef768
Rushikesh Tarate <78791416+RushikeshGit-System@users.noreply.github.com>
2023-12-31 19:25:24 +0530

    Add files via upload

---
+++ productBarCodeGenerator.js
@@ -0,0 +1,31 @@
+import { LightningElement,api } from 'lwc';
+import { loadScript } from 'lightning/platformResourceLoader';
+import barcode from "@salesforce/resourceUrl/barcode"; 
+
+export default class ProductBarCodeGenerator extends LightningElement {
+    @api recordId;
+    renderedCallback() {
+        Promise.all([
+            loadScript(this, barcode)
+        ]).then(() =>{
+            console.log('script loaded');
+            this.renderButtons();
+            //generateBarcode();
+        }).catch(error => {
+            window.console.log("Error " + error);
+        });
+    }
+
+    renderButtons(){
+        
+        this.boolShowSpinner = false;
+    }
+    generateBarcode(){
+        const canvas = this.template.querySelector('[data-id="barcode"]');  
+        JsBarcode(canvas, this.recordId, {
+            format: "CODE39"
+          });        
+          JsBarcode(".barcode").init(); 
+    }  
+
+}
